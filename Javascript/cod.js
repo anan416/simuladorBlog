ed6d199cf07d019d4533c9f5fe8357d32046483d
anan416 <90143779+anan416@users.noreply.github.com>
2021-12-06 18:26:18 -0300

    Add files via upload

--- Javascript/cod.js
+++ Javascript/cod.js
@@ -1,4 +1,49 @@
-function comentar(){
-    var comentario = document.querySelector(".comment");
-    alert(comentario);
+function comentar1(){
+    var comentario = document.querySelector(".casilla1").value;
+    document.querySelector(".comment1").innerHTML = "Comentario anónimo: " + comentario;
+}
+
+function comentar2(){
+    var comentario = document.querySelector(".casilla2").value;
+    document.querySelector(".comment2").innerHTML = "Comentario anónimo: " + comentario;
+}
+
+function comentar3(){
+    var comentario = document.querySelector(".casilla3").value;
+    document.querySelector(".comment3").innerHTML = "Comentario anónimo: " + comentario;
+}
+
+function comentar4(){
+    var comentario = document.querySelector(".casilla4").value;
+    document.querySelector(".comment4").innerHTML = "Comentario anónimo: " + comentario;
+}
+
+function comentar5(){
+    var comentario = document.querySelector(".casilla5").value;
+    document.querySelector(".comment5").innerHTML = "Comentario anónimo: " + comentario;
+}
+
+function comentar6(){
+    var comentario = document.querySelector(".casilla6").value;
+    document.querySelector(".comment6").innerHTML = "Comentario anónimo: " + comentario;
+}
+
+function comentar7(){
+    var comentario = document.querySelector(".casilla7").value;
+    document.querySelector(".comment7").innerHTML = "Comentario anónimo: " + comentario;
+}
+
+function comentar8(){
+    var comentario = document.querySelector(".casilla8").value;
+    document.querySelector(".comment8").innerHTML = "Comentario anónimo: " + comentario;
+}
+
+function comentar9(){
+    var comentario = document.querySelector(".casilla9").value;
+    document.querySelector(".comment9").innerHTML = "Comentario anónimo: " + comentario;
+}
+
+function comentar0(){
+    var comentario = document.querySelector(".casilla0").value;
+    document.querySelector(".comment0").innerHTML = "Comentario anónimo: " + comentario;
 }
