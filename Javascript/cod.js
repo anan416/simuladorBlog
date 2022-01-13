66e58faf4b70b71332d01a3035ae42e46256ec95
anan416 <90143779+anan416@users.noreply.github.com>
2022-01-13 12:07:45 -0300

    Add files via upload

--- Javascript/cod.js
+++ Javascript/cod.js
@@ -1,49 +1,110 @@
+comentarios1 = []
+comentarios2 = []
+comentarios3 = []
+comentarios4 = []
+comentarios5 = []
+comentarios6 = []
+comentarios7 = []
+comentarios8 = []
+comentarios9 = []
+comentarios0 = []
+
 function comentar1(){
     var comentario = document.querySelector(".casilla1").value;
-    document.querySelector(".comment1").innerHTML = "Comentario anónimo: " + comentario;
+    mensaje = "Comentario anónimo: " + comentario + "<br>";
+    comentarios1.push(mensaje);
+    document.querySelector(".comment1").innerHTML = ""
+    for (var i = 0; i < comentarios1.length; i++) {
+        document.querySelector(".comment1").innerHTML += comentarios1[i];  
+    }   
 }
 
 function comentar2(){
     var comentario = document.querySelector(".casilla2").value;
-    document.querySelector(".comment2").innerHTML = "Comentario anónimo: " + comentario;
+    mensaje = "Comentario anónimo: " + comentario + "<br>";
+    comentarios2.push(mensaje);
+    document.querySelector(".comment2").innerHTML = ""
+    for (var i = 0; i < comentarios2.length; i++) {
+        document.querySelector(".comment2").innerHTML += comentarios2[i];  
+    }   
 }
 
 function comentar3(){
     var comentario = document.querySelector(".casilla3").value;
-    document.querySelector(".comment3").innerHTML = "Comentario anónimo: " + comentario;
+    mensaje = "Comentario anónimo: " + comentario + "<br>";
+    comentarios3.push(mensaje);
+    document.querySelector(".comment3").innerHTML = ""
+    for (var i = 0; i < comentarios3.length; i++) {
+        document.querySelector(".comment3").innerHTML += comentarios3[i];  
+    }   
 }
 
 function comentar4(){
     var comentario = document.querySelector(".casilla4").value;
-    document.querySelector(".comment4").innerHTML = "Comentario anónimo: " + comentario;
+    mensaje = "Comentario anónimo: " + comentario + "<br>";
+    comentarios4.push(mensaje);
+    document.querySelector(".comment4").innerHTML = ""
+    for (var i = 0; i < comentarios4.length; i++) {
+        document.querySelector(".comment4").innerHTML += comentarios4[i];  
+    }   
 }
 
 function comentar5(){
     var comentario = document.querySelector(".casilla5").value;
-    document.querySelector(".comment5").innerHTML = "Comentario anónimo: " + comentario;
+    mensaje = "Comentario anónimo: " + comentario + "<br>";
+    comentarios5.push(mensaje);
+    document.querySelector(".comment5").innerHTML = ""
+    for (var i = 0; i < comentarios5.length; i++) {
+        document.querySelector(".comment5").innerHTML += comentarios5[i];  
+    }   
 }
 
 function comentar6(){
     var comentario = document.querySelector(".casilla6").value;
-    document.querySelector(".comment6").innerHTML = "Comentario anónimo: " + comentario;
+    mensaje = "Comentario anónimo: " + comentario + "<br>";
+    comentarios6.push(mensaje);
+    document.querySelector(".comment6").innerHTML = ""
+    for (var i = 0; i < comentarios6.length; i++) {
+        document.querySelector(".comment6").innerHTML += comentarios6[i];  
+    }   
 }
 
 function comentar7(){
     var comentario = document.querySelector(".casilla7").value;
-    document.querySelector(".comment7").innerHTML = "Comentario anónimo: " + comentario;
+    mensaje = "Comentario anónimo: " + comentario + "<br>";
+    comentarios7.push(mensaje);
+    document.querySelector(".comment7").innerHTML = ""
+    for (var i = 0; i < comentarios7.length; i++) {
+        document.querySelector(".comment7").innerHTML += comentarios7[i];  
+    }   
 }
 
 function comentar8(){
     var comentario = document.querySelector(".casilla8").value;
-    document.querySelector(".comment8").innerHTML = "Comentario anónimo: " + comentario;
+    mensaje = "Comentario anónimo: " + comentario + "<br>";
+    comentarios8.push(mensaje);
+    document.querySelector(".comment8").innerHTML = ""
+    for (var i = 0; i < comentarios8.length; i++) {
+        document.querySelector(".comment8").innerHTML += comentarios8[i];  
+    }   
 }
 
 function comentar9(){
     var comentario = document.querySelector(".casilla9").value;
-    document.querySelector(".comment9").innerHTML = "Comentario anónimo: " + comentario;
+    mensaje = "Comentario anónimo: " + comentario + "<br>";
+    comentarios9.push(mensaje);
+    document.querySelector(".comment9").innerHTML = ""
+    for (var i = 0; i < comentarios9.length; i++) {
+        document.querySelector(".comment9").innerHTML += comentarios9[i];  
+    }   
 }
 
 function comentar0(){
     var comentario = document.querySelector(".casilla0").value;
-    document.querySelector(".comment0").innerHTML = "Comentario anónimo: " + comentario;
+    mensaje = "Comentario anónimo: " + comentario + "<br>";
+    comentarios0.push(mensaje);
+    document.querySelector(".comment0").innerHTML = ""
+    for (var i = 0; i < comentarios0.length; i++) {
+        document.querySelector(".comment0").innerHTML += comentarios0[i];  
+    }   
 }
